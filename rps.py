da5502fab12ae56c3393a28a29c7142fa268740d
Дмитрий Воронецкий <vda1999@yandex.ru>
2023-08-20 11:48:40 +0400

    adding requester

---
+++ rps.py
@@ -0,0 +1,31 @@
+import signal
+import socket
+from sys import argv
+
+
+class TimeoutException(Exception):   # Custom exception class
+    pass
+
+
+def timeout_handler(signum, frame):   # Custom signal handler
+    raise TimeoutException
+
+
+# Change the behavior of SIGALRM
+signal.signal(signal.SIGALRM, timeout_handler)
+
+if __name__ == "__main__":
+    while True:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect(('localhost', 8002))
+            signal.alarm(1)
+            executed = 0
+            while True:
+                try:
+                    s.send(argv[1].encode('utf-8') + b'\n')
+                    s.recv(100)
+                    executed += 1
+                except TimeoutException:
+                    print("last rps: ", executed)
+                    signal.alarm(0)
+                    break
